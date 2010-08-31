00941777cb9158c767b8be2f5889e015c3e90969
Alex Eftimie <alex@eftimie.ro>
2010-08-31 19:32:01 +0300

    added basic moodle scrapper using cURL. works like a charm with http://cs.curs.pub.ro

---
+++ middleware/scrapper/moodle.py
@@ -0,0 +1,85 @@
+# A scrapper class connecting to moodle and fetching data
+# Uses cURL as backend
+
+import os, sys
+import curl, pycurl
+
+class MoodleSession(curl.Curl):
+    """ Moodle-specific methods. Sensitive to changes in site design. """
+    def login(self, name, password):
+        """Establish a login session."""
+        self.post("login/index.php", (("username", name),
+                                        ("password", password),
+                                        ))
+    def logout(self):
+        """Log out 
+        TODO
+            - find more about the sesskey problem
+        """
+        self.get("login/logout.php")
+        
+    def list_courses(self):
+        """ Get a list of courses. To be called right after login (/my/)
+        TODO: 
+            - make a get call before this
+            - better parsing
+        """
+        ids = []
+        content = self.body()
+        body = content[content.find('id="content"'):]
+        index = 0
+        
+        while True:
+            index = body.find('/course/view.php?id=', index + 1)
+            if index == -1: break
+            id = body[index + 20:body.find('"', index + 1)]
+            name = body[index + 22 + len(id):body.find('<', index + 1)]
+            ids.append((int(id), name))
+        #print "Course ids: ", ids
+        return ids
+        
+    def list_groups(self, course_id):
+        """ Get the list of groups and students inside
+        TODO:
+            - fix unicode bug at student's name
+        """
+        self.get("group/overview.php?id=%d" % course_id)
+        content = self.body()
+        body = content[content.find('id="selectgroup_jump"'):]
+        
+        # Get group name/ids
+        index = 0
+        last_index = 0
+        groups = {}
+        while True:
+            index = body.find('group=', index + 1)
+            if index == -1: break
+            id = body[index + 6:body.find('"', index + 1)]
+            end_index = body.find('>', index + 6 + len(id))
+            name = body[end_index + 1:body.find('<', end_index)]
+            if id != '0' and id != '':
+                groups[id] = {"name": name, "users": None}
+            if index != -1:
+                last_index = index
+                
+        # Get members
+        body = body[last_index + 20:]
+        for id, data in groups.items():
+            index = body.find(data['name'])
+            end = body.find('<tr ', index)
+            userstext = body[index:end]
+            users = []
+            j = 0
+            while True:
+                j = userstext.find('/user/view.php?id=', j + 20)
+                if j == -1: break
+                id = int(userstext[j + 18:userstext.find('&', j + 18)])
+                name = userstext[userstext.find('>', j) + 1:userstext.find('<', j)]
+                users.append((id, name, self.get_avatar(id)))
+            data['users'] = users
+            
+        return groups
+        
+    def get_avatar(self, user_id):
+        """ Return user's avatar url """
+        return "%suser/pix.php/%d/f1.jpg" % (self.base_url, user_id)
---
+++ middleware/worker.py
@@ -0,0 +1,43 @@
+# A worker process connecting to moodle and fetching data
+# Uses cURL as backend
+
+import os, sys
+import curl, pycurl
+
+from scrapper.moodle import MoodleSession
+
+MOODLE_URL = "https://cs09.curs.pub.ro/"
+USER = "ioan.eftimie"
+PASS = "***"
+
+COURSE_ID = 9
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 3:
+        USER, PASS = sys.argv[1:3]
+        if len(sys.argv) >= 4:
+            MOODLE_URL = sys.argv[3]
+    else:
+        print "Usage: %s <user> <password> [host]" % sys.argv[0]
+        
+    session = MoodleSession(MOODLE_URL)
+    session.set_verbosity(0)
+    
+    try:
+        session.login(USER, PASS)
+    except pycurl.error:
+        sys.stderr.write("Failed to connect.\n")
+        sys.exit(1)
+    
+    if session.answered("Overview of my courses"):
+        print session.list_courses()
+        print session.list_groups(COURSE_ID)
+        session.logout()
+        sys.exit(0)
+    elif session.answered("forgot_password.php"):
+        sys.stderr.write("Wrong username/password\n")
+        sys.exit(-1)
+    else:
+        sys.stderr.write("Unexpected page (%d bytes)\n" % len(session.body()))
+        #sys.stdout.write(session.body())
+        sys.exit(-2)
