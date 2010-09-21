2d4af1f88621759568cefecad48b51c056834537
Sergiu <me@serg.ro>
2010-09-22 01:47:43 +0200

    Grade support bugfix

--- middleware/rest/views.py
+++ middleware/rest/views.py
@@ -116,11 +116,13 @@ def group(request, user, session_key, name, course, activity_id):
     
     try:
         group = Group.objects.get(name=name, course__name=course)
-        attendance, created = Attendance.objects.get_or_create(course = course, student = s, activity = act, week = get_week(act.day_start), defaults={'grade': 0})
-        students = [ {"name": s.name, 
+        students = []
+        for s in group.students.all():
+            attendance, created = Attendance.objects.get_or_create(course = course, student = s, activity = act, week = get_week(act.day_start), defaults={'grade': 0})
+            students.append({"name": s.name, 
                 "grade": attendance.grade,
                 "id": s.id,
-                "avatar": s.avatar} for s in group.students.all() ]
+                "avatar": s.avatar})
     except Group.DoesNotExist:
         return json_response({"error": "No such group"}, failed = True)
                 
@@ -145,11 +147,13 @@ def current_group(request, user, session_key, course):
             end = datetime.time(act.time_hour_end, act.time_minute_end)
             today = datetime.date.today().weekday()
             if today == act.day and start <= now and now <= end:
-                attendance, created = Attendance.objects.get_or_create(course = course, student = s, activity = act, week = get_week(act.day_start), defaults={'grade': 0})
-                students = [ {"name": s.name, 
-                    "grade": attendance.grade,
-                    "id": s.id,
-                    "avatar": s.avatar} for s in group.students.all() ]    
+                students = []
+                for s in group.students.all(): 
+                    attendance, created = Attendance.objects.get_or_create(course = course, student = s, activity = act, week = get_week(act.day_start), defaults={'grade': 0})
+                    students.append({"name": s.name, 
+                        "grade": attendance.grade,
+                        "id": s.id,
+                        "avatar": s.avatar})
                 return json_response({"name": group.name, "students": students, 'activity_id' : act.id})
     return json_response({"error":"no current group"}, failed = True)
     
