d00fe4da9700968067fbf55caba050d94de266aa
Sergiu <me@serg.ro>
2010-09-22 00:53:06 +0200

    More bugfixing on save

--- middleware/rest/views.py
+++ middleware/rest/views.py
@@ -224,21 +224,24 @@ def post_data(request):
     except Course.DoesNotExist:
         return json_response({"error":"No such course"}, failed = True)
 
-    data = json.loads(request.POST['contents'])
-    if request.POST['type'] == 'group':
-        try:
-            act = Activity.objects.get(id = data['activity_id'])
-        except Activity.DoesNotExist:
-            return json_response({"error":"Activity not found"}, failed = True)
-        for student in data['students']:
-            try: 
-                current_student = Student.objects.get(id = student['id'])
-                attendance, created = Attendance.objects.get_or_create(course = course, student = current_student, activity = act, week = get_week(act.day_start), defaults={'grade': student['grade']})
-                attendance.grade = student['grade']
-                attendance.save()
-            except Student.DoesNotExist:
-                return json_response({"error":"Student not found"}, failed = True)
-        return json_response({})
-    else:
+    try:
+        data = json.loads(request.POST['contents'])
+        if request.POST['type'] == 'group':
+            try:
+                act = Activity.objects.get(id = data['activity_id'])
+            except Activity.DoesNotExist:
+                return json_response({"error":"Activity not found"}, failed = True)
+            for student in data['students']:
+                try: 
+                    current_student = Student.objects.get(id = int(student['id']))
+                    student['grade'] = float(student['grade'])
+                    attendance, created = Attendance.objects.get_or_create(course = course, student = current_student, activity = act, week = get_week(act.day_start), defaults={'grade': student['grade']})
+                    attendance.grade = student['grade']
+                    attendance.save()
+                except Student.DoesNotExist:
+                    return json_response({"error":"Student not found"}, failed = True)
+            return json_response({})
+        else:
+            return json_response({"error":"Wrong query type"}, failed = True)
+    except Exception:
         return json_response({"error":"Wrong query type"}, failed = True)
-    
